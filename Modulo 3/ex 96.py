96161fc3c88412634410b00ad49d1289d715087e
Matheus_Hslv <89474499+mhsilvaworks@users.noreply.github.com>
2025-07-07 15:55:59 -0300

    ex 96

--- Modulo 3/ex 96.py
+++ Modulo 3/ex 96.py
@@ -1,4 +1,8 @@
+# Calculador de área utiliizando funções
+def funcao(largura, comprimento):
+    print(f'A área de um terreno de {largura} m de largura por {comprimento} m de comprimento é {largura*comprimento} metros.')
 
-
-x = 'World'
-print(f'Hello, {x:.>20}!!') 
+a = float(input('Informe a largura: '))
+b = float(input('Informe o comprimento: '))
+          
+funcao(a,b)
