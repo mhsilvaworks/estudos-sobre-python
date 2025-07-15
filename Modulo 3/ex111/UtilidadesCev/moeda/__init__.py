c5e5bf0a7bb7bd1abbc0d7301bffdf3f3f9859d3
Matheus_Hslv <89474499+mhsilvaworks@users.noreply.github.com>
2025-07-15 13:21:05 -0300

    Add files via upload

---
+++ Modulo 3/ex111/UtilidadesCev/moeda/__init__.py
@@ -0,0 +1,24 @@
+def aumentar(v=0, p=0, format= False):
+    r=(v*((100+p)/100))  
+    return r if format is False else moeda (r)
+def diminuir(v=0, p=0,format= False):
+    r= v*((100-p)/100)
+    return r if format is False else moeda (r)
+def dobro(v=0,format= False):
+    r  = v*2
+    return r if format is False else moeda (r)
+def metade(v=0,format= False):
+    r = v/2
+    return r if format is False else moeda (r)
+def moeda(p= 0, m ='R$',format= False):
+    p = float(p)
+    return f'{m}{p:.2f}'.replace('.',',') 
+def resumo (x,a,b):
+    print('='*35)
+    print(f'{'RESUMO':>20}')
+    print('='*35)
+    print(f'{'Se aumentar 10%:':<20} {aumentar(x,a,True)}')
+    print(f'{'Se diminuir 10%:':<20} {diminuir(x,b, True)}')
+    print(f'{'Se dobrar:':<20} {dobro(x, True)}')
+    print(f'{'Se dividir por 2:':<20} {metade(x, True)}')
+    print('='*35)
