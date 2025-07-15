d09b17fb1f5f9764ba5bccc678ff3e3c1d97224b
Matheus_Hslv <89474499+mhsilvaworks@users.noreply.github.com>
2025-07-14 21:49:53 -0300

    Add files via upload

---
+++ Modulo 3/ex 107.py/Programaprincipal.py
@@ -0,0 +1,6 @@
+import pasta
+x = int(input('Informe algum valor: '))
+print(f'Se aumentar 10%: {pasta.aumentar(x,10)}')
+print(f'Se diminuir 10%: {pasta.diminuir(x,10)}')
+print(f'Se dobrar: {pasta.dobro(x)}')
+print(f'Se dividir por 2: {pasta.metade(x)}')
---
+++ Modulo 3/ex 107.py/pasta.py
@@ -0,0 +1,12 @@
+def aumentar(v, p):
+    r=int(v*((100+p)/100))  
+    return r
+def diminuir(v, p):
+    r= v*((100-p)/100)
+    return r, p
+def dobro(v):
+    r  = v*2
+    return r
+def metade(v):
+    r = v/2
+    return r
